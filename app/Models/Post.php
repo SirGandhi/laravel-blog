d77e2ba6845ad5fe1cef2ea10363ab540b4cd2be
SirGandhi <it4luke@outlook.com>
2022-03-17 19:28:39 +0000

    Lesson 11 - Complete

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,13 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
 
 class post {
+    public static function allPosts() {
+        $files = File::files(resource_path("posts/"));
+        return array_map(function ($file) {
+            return $file->getContents();
+        }, $files);
+    }
     public static function find($slug) {
         if(! file_exists($path = resource_path("posts/{$slug}.html"))) {
             throw new ModelNotFoundException();
         }
-        return cache()->remember("posts.{$slug}", 1200, function () use ($path) {
+        return cache()->remember("posts.{$slug}", 0, function () use ($path) {
             return file_get_contents($path);
         });
     }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,17 +5,10 @@
         <title>My Blog</title>
     </head>
     <body>
-        <article>
-            <h1><a href="/posts/blog-one">My First Post Test</a></h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </article>
-        <article>
-            <h1><a href="/posts/blog-two">My Second Post</a></h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </article>
-        <article>
-            <h1><a href="/posts/blog-three">My Third Post</a></h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </article>
+        @foreach ($posts as $single_post)
+            <article>
+                {!! $single_post !!}
+            </article>
+        @endforeach
     </body>
 </html>
