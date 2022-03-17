8f5fd534d01348a06bb1241bdf9d5c82a84bb49e
SirGandhi <it4luke@outlook.com>
2022-03-17 14:35:22 +0000

    First commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="/css/app.css">
+        <title>My Blog</title>
+    </head>
+    <body>
+        <article>
+            <?= $post; ?>
+        </article>
+    </body>
+</html>
